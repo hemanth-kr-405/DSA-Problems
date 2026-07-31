413f38171c107c5a9e45406677fb7060a454ed3d
Hemanth K R <hemanth1036kr@gmail.com>
2026-07-31 12:03:54 +0530

    Time: 3 ms (44.41%) | Memory: 8.5 MB (92.85%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int n = x;
+        long long rev = 0;
+        while(n > 0){
+            int digits = n % 10;
+            
+            rev = rev * 10 + digits;
+            n = n/10;
+        }
+        if(rev == x){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
